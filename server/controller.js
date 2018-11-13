123a74afbe330aa59391f3dd0afbab7e59bc96d0
randaman23 <randaman234@gmail.com>
2018-11-13 11:53:02 -0700

    create user working on backend, with session working

--- server/controller.js
+++ server/controller.js
@@ -8,7 +8,7 @@ module.exports = {
     if (foundUser[0]) return res.status(409).send("Email already exists");
     let salt = bcrypt.genSaltSync(10);
     let hash = bcrypt.hashSync(password, salt);
-    let user = db.create_user([email, hash]);
+    let user = await db.create_user([email, hash]);
     req.session.user = user[0];
     res.status(200).send(req.session.user);
   }
