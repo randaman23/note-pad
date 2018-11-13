d9707f898e70701d4ede00aba69adf73f30b7d5a
randaman23 <randaman234@gmail.com>
2018-11-13 10:36:45 -0700

    some css added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app_main">
         <HashRouter>
           <Switch>
             <Route path="/" exact component={Home} />
