438059e7857a2f60eab2a38865061962f8f24bdd
randaman23 <randaman234@gmail.com>
2018-11-13 15:52:38 -0700

    trying to style home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
-        <Link to="/login">
-          <button>Sign In</button>
-        </Link>
-        <Link to="/register">
-          <button>Register</button>
-        </Link>
+      <div className="home_main">
+        <div className="home_header">
+          <h1>Thought Jotter</h1>
+        </div>
+        <div className="pushdown"></div>
+        <div></div>
+        <div className="signin">
+          <h1>Login or Register</h1>
+          <Link to="/login">
+            <button>Sign In</button>
+          </Link>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
       </div>
     );
   }
